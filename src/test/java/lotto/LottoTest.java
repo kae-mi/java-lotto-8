390250d388345a791997482ecf94c3fad22b7303
kae-mi <rudahyang7815@gmail.com>
2025-11-01 22:50:36 +0900

    feat(Lotto): 로또 번호, 로또 생성 및 검증 기능 구현 - Lotto 객체 생성, 생성 과정에서의 번호 검증 - 로또 번호 자동 랜덤 생성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,10 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +23,38 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1부터 45 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_1부터_45_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 0 이하면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_0_이하면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호는 오름차순으로 정렬된다.")
+    @Test
+    void 로또_번호는_오름차순으로_정렬된다() {
+        Lotto lotto = new Lotto(Arrays.asList(6, 5, 4, 3, 2, 1));
+        assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
+    }
+
+    @DisplayName("자동 생성된 로또는 6개의 번호를 가진다.")
+    @Test
+    void 자동_생성된_로또는_6개의_번호를_가진다() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        assertThat(lotto.getNumbers()).hasSize(6);
+    }
+
+    @DisplayName("자동 생성된 로또의 번호는 1부터 45 사이다.")
+    @Test
+    void 자동_생성된_로또의_번호는_범위_내에_있다() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        assertThat(lotto.getNumbers()).allMatch(number -> number >= 1 && number <= 45);
+    }
 }
