3c9544fd7af8fa8298487e1a532c97c8eaaac8d5
kae-mi <rudahyang7815@gmail.com>
2025-11-02 22:22:35 +0900

    feat(OutputView): 당첨 통계 및 수익률 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoPaper;
+import lotto.domain.Ranking;
+import lotto.domain.WinningStatistics;
 
 import java.util.List;
 
@@ -9,6 +11,10 @@ public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.";
     private static final String LOTTO_NUMBERS_FORMAT = "[%s]";
     private static final String NUMBER_DELIMITER = ", ";
+    private static final String STATISTICS_HEADER = "당첨 통계";
+    private static final String STATISTICS_DIVIDER = "---";
+    private static final String RANK_RESULT_FORMAT = "%s (%s원) - %d개";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public void printPurchaseCount(int count) {
         System.out.println();
@@ -31,5 +37,41 @@ public class OutputView {
                 .map(String::valueOf)
                 .reduce((a, b) -> a + NUMBER_DELIMITER + b)
                 .orElse("");
+    }
+
+    public void printWinningStatistics(WinningStatistics statistics) {
+        printStatisticsHeader();
+        printRankStatistics(statistics);
+    }
+
+    private void printStatisticsHeader() {
+        System.out.println();
+        System.out.println(STATISTICS_HEADER);
+        System.out.println(STATISTICS_DIVIDER);
+    }
+
+    private void printRankStatistics(WinningStatistics statistics) {
+        printRankResult(Ranking.FIFTH, statistics.getCountByRank(Ranking.FIFTH));
+        printRankResult(Ranking.FOURTH, statistics.getCountByRank(Ranking.FOURTH));
+        printRankResult(Ranking.THIRD, statistics.getCountByRank(Ranking.THIRD));
+        printRankResult(Ranking.SECOND, statistics.getCountByRank(Ranking.SECOND));
+        printRankResult(Ranking.FIRST, statistics.getCountByRank(Ranking.FIRST));
+    }
+
+    private void printRankResult(Ranking ranking, int count) {
+        String formattedPrizeMoney = formatPrizeMoney(ranking.getPrizeMoney());
+        System.out.println(String.format(RANK_RESULT_FORMAT,
+                ranking.getDescription(),
+                formattedPrizeMoney,
+                count));
+    }
+
+    private String formatPrizeMoney(int prizeMoney) {
+        return String.format("%,d", prizeMoney);
+    }
+
+    public void printRateOfReturn(double rateOfReturn) {
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
+        System.out.println();
     }
 }
